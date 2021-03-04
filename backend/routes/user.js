11be863d20adbe7606778055b2542093f3259f66
Michael <mishatolchinski@gmail.com>
2021-03-04 18:06:38 +0200

    .

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,4 +5,4 @@ const router = express.Router();
 router.post('/signup',userController.createUser);
 router.post('/login',userController.userLogin);
 
-module.exports = router
+module.exports = router;
