294a14911fe2a0bc6f5330ec61e1fdd198fe5a75
Michael Tolchinsky <mishatolchinski@gmail.com>
2022-03-17 10:19:33 +0200

    .

--- backend/app.js
+++ backend/app.js
@@ -27,6 +27,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Methods",
     "GET, POST, PATCH, PUT, DELETE, OPTIONS"
   );
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   next();
 });
 
