ad07790db29d74428a7683b9c38a5b3ae3f9d94d
Michael Tolchinsky <mishatolchinski@gmail.com>
2023-07-22 22:13:50 +0300

    local reroute to go backend

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: false,
-  //apiUrl:'http://localhost:3000/api', node.js express api
-  apiUrl: 'http://localhost:3001/api', // golang fiber api
+  apiUrl:'http://localhost:3000/api',
 };
 
 /*
