97fcd19e708ffb4d0e212deed795ae0d7f5014c7
Michael Tolchinsky <mishatolchinski@gmail.com>
2023-07-23 20:54:55 +0300

    fixed local route to 3001

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl:'http://localhost:3000/api',
+  //apiUrl: 'http://localhost:3000/api', // node backend
+  apiUrl: 'http://localhost:3001/api', // golang backend
 };
 
 /*
