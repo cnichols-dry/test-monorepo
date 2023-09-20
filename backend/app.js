82199efe2945fc9d1ebd3ee121e4ca8ce0a2275c
cnichols-dry <142335019+cnichols-dry@users.noreply.github.com>
2023-09-20 12:12:41 -0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const userRoutes = require('./routes/user');
 const app = express();
 app.use(cors())
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+// app.use(express.urlencoded({ extended: false }))
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_ATLAS_USER}:${process.env.MONGO_ATLAS_PW}@${process.env.MONGO_ATLAS_CLUSTER}/${process.env.MONGO_DB_NAME}`,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
     console.log('Connected to Databse');
